91a6ab829a4d2d3e37b17ad755241819316e3bc5
qrxnz <send@qrxnz.dev>
2024-09-12 15:26:45 +0200

    tmux fix

--- setup-non-nixos.sh
+++ setup-non-nixos.sh
@@ -4,4 +4,4 @@ stow files --adopt
 
 echo "source ~/.config/zshrc/zshrc" > ~/.zshrc
 
-echo "source-file ~/.config/tmux/tmux.conf" > ~/.zshrc
+echo "source-file ~/.config/tmux/tmux.conf" > ~/.tmux.conf
