b60a58e74cafea3e1f40e442e8fdef74301d1522
qrxnz <send@qrxnz.dev>
2024-10-07 18:32:57 +0200

    fixed host

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.mentay = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
         ./snowy/hosts/mentay
