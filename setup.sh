053c0a391eeadb6a984506b381c85bab136a8137
qrxnz <send@qrxnz.dev>
2024-10-09 12:46:33 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,36 @@
- #!/usr/bin/env bash
+#!/usr/bin/env bash
 
-# Sprawdzenie, czy argument --hyprland-default został podany
 if [[ "$1" == "--hyprland-default" ]]; then
     stow files --adopt
 
     echo "source ~/.config/zshrc/zshrc" > ~/.zshrc
     echo "source-file ~/.config/tmux/tmux.conf" > ~/.tmux.conf
+    
+    cd $HOME
+    echo "Installation Completed!"
+
+elif [[ "$1" == "--kali-linux" ]]; then
+    sudo apt update && sudo apt upgrade -y
+    sudo apt-get install -y wget curl git thunar stow neovim podman zoxide fzf \
+    xsel xclip duf eza bat flameshot feh kitty tmux picom \
+    golang delve clang ccls gdb cargo
+
+    mkdir -p ~/.local/bin/
+    curl -fsSL https://bun.sh/install | bash
+    curl -sS https://starship.rs/install.sh | sh
+    sh <(curl -L https://nixos.org/nix/install) --no-daemon
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+    
+    stow files --adopt --ignore=hyprland --ignore=waybar
+    
+    echo "source ~/.config/zshrc/zshrc" > ~/.zshrc
+    echo "source-file ~/.config/tmux/tmux.conf" > ~/.tmux.conf
+    
+    cd $HOME
+    echo "Installation Completed!"
+
 else
-    echo "Invalid argument"
+    echo "Invalid argument. Try: ./setup.sh --hyprland-default or ./setup.sh --kali-linux"
     exit 1
 fi
