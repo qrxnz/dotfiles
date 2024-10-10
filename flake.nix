cbb3cc452b3b2beac293cf9a46390b4f7ba4aede
qrxnz <send@qrxnz.dev>
2024-10-10 16:50:52 +0200

    flake up

--- flake.nix
+++ flake.nix
@@ -1,22 +1,80 @@
 {
-  description = "Nixos config flake";
+  description = " qrxnz's NixOS configuration";
+
+  nixConfig = {
+    # substituers will be appended to the default substituters when fetching packages
+    # nix com    extra-substituters = [munity's cache server
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+    catppuccin-bat = {
+      url = "github:catppuccin/bat";
+      flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.mentay = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./snowy/hosts/mentay
-        inputs.home-manager.nixosModules.default
-      ];
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  }: {
+    nixosConfigurations = {
+      mentay = let
+        username = "qrxnz";
+        specialArgs = {inherit username;};
+      in
+        nixpkgs.lib.nixosSystem {
+          inherit specialArgs;
+          system = "x86_64-linux";
+
+          modules = [
+            ./snowy/hosts/mentay
+            # ./users/${username}/nixos.nix
+
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+
+              home-manager.extraSpecialArgs = inputs // specialArgs;
+              home-manager.users.${username} = import ./users/${username}/home.nix;
+            }
+          ];
+        };
+
+      msi-rtx4090 = let
+        username = "qrxnz"; # another username for this machine
+        specialArgs = {inherit username;};
+      in
+        nixpkgs.lib.nixosSystem {
+          inherit specialArgs;
+          system = "x86_64-linux";
+
+          modules = [
+            ./snowy/hosts/mentay
+            # ./users/${username}/nixos.nix
+
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+
+              home-manager.extraSpecialArgs = inputs // specialArgs;
+              home-manager.users.${username} = import ./users/${username}/home.nix;
+            }
+          ];
+        };
     };
   };
 }
---
+++ snowy/users/home.nix
@@ -0,0 +1,18 @@
+{pkgs, ...}: {
+
+#  imports = [
+#    ../../home/core.nix
+#
+#    ../../home/fcitx5
+#    ../../home/i3
+#    ../../home/programs
+#    ../../home/rofi
+#    ../../home/shell
+#  ];
+
+  programs.git = {
+    userName = "qrxnz";
+    userEmail = "send@qrxnz.dev";
+  };
+}
+
