258e7ae3b2ffe88beb35dac90810170b42f97cce
qrxnz <send@qrxnz.dev>
2024-10-11 23:01:11 +0200

    vscodium

---
+++ snowy/home/vscode/default.nix
@@ -0,0 +1,16 @@
+{
+  pkgs,
+  config,
+  username,
+  ...
+}: {
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    extensions = with pkgs.vscode-extensions; [
+      dracula-theme.theme-dracula
+      vscodevim.vim
+      yzhang.markdown-all-in-one
+    ];
+  };
+}
--- snowy/users/qrxnz/home.nix
+++ snowy/users/qrxnz/home.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   imports = [
     ../../home/core.nix
+    ../../home/vscode
   ];
 
   programs.git = {
