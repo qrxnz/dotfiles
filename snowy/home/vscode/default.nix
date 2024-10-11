974db39767ecec96cada9007a5b7e3c05f1f6161
qrxnz <send@qrxnz.dev>
2024-10-11 23:22:24 +0200

    vscode setup

--- snowy/home/vscode/default.nix
+++ snowy/home/vscode/default.nix
@@ -8,9 +8,33 @@
     enable = true;
     package = pkgs.vscodium;
     extensions = with pkgs.vscode-extensions; [
-      dracula-theme.theme-dracula
-      vscodevim.vim
-      yzhang.markdown-all-in-one
+
+      #
+      # Theme
+      #
+
+
+      ########### The best like Polyphia the worst :P ####
+      catppuccin.catppuccin-vsc ##########################
+      catppuccin.catppuccin-vsc-icons ## catppuccin theme#
+      ####################################################
+
+      #
+      # Language extensions
+      #
+
+      ms-python.python # python
+      ms-vscode.vscode-typescript-next # js/ts
+      golang.go # golang
+      llvm-vs-code-extensions.vscode-clangdS # c/c++
+
+      #
+      # Others
+      #
+
+      vscodevim.vim # vim support for vscode
+      yzhang.markdown-all-in-one # markdown shit
+      pixl-garden.bongocat # yes, I like cats
     ];
   };
 }
