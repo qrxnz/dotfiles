3decd33a6e63f6a01f3a33508ec4871293bb0c5f
qrxnz <send@qrxnz.dev>
2024-10-11 15:52:16 +0200

    flatpak module

--- snowy/hosts/mentay/default.nix
+++ snowy/hosts/mentay/default.nix
@@ -7,6 +7,7 @@
       ../../suites/common
       ../../suites/desktop
       ../../home
+      ../../modules/flatpak
       ../../modules/virtualisation/qemu
       ../../modules/virtualisation/podman
       ../../modules/virtualisation/waydroid
@@ -25,7 +26,6 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  services.flatpak.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
