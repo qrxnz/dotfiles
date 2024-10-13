03f1d36dd202ca5125916d93f7aee3bb5d3cd51f
qrxnz <send@qrxnz.dev>
2024-10-13 16:22:44 +0200

    debloat

--- snowy/hosts/mentay/default.nix
+++ snowy/hosts/mentay/default.nix
@@ -14,22 +14,13 @@
     ];
 
   # Hostname
-  networking.hostName = "mentay"; # Define your hostname.
-
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.hostName = "mentay";
 
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "pl";
     variant = "";
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Install firefox.
   programs.firefox.enable = true;
