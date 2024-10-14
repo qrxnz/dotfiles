4894e4c856052404ea9c8e11cfa11179a5f1763c
qrxnz <send@qrxnz.dev>
2024-10-14 11:31:07 +0200

    default prompt

--- snowy/home/shell/default.nix
+++ snowy/home/shell/default.nix
@@ -37,22 +37,26 @@
        )
        '';
        shellAliases = {
-        vi = "hx";
-        vim = "hx";
-        nano = "hx";
+        v = "nvim";
+        vi = "nvim";
+        vim = "nvim";
+        nano = "nvim";
+
+        vsc = "codium";
        };
    };  
    carapace.enable = true;
    carapace.enableNushellIntegration = true;
 
-   starship = { enable = true;
+   starship = { 
+    enable = true;
        settings = {
          add_newline = true;
-         character = { 
-         success_symbol = "[➜](bold green)";
-         error_symbol = "[➜](bold red)";
-          };
-        };
+         #character = { 
+         #success_symbol = "[➜](bold green)";
+         #error_symbol = "[➜](bold red)";
+        #};
       };
     };
+  };
 }
