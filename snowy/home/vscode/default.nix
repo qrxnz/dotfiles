e7f19fc01c7a3cd9cd5d07ccb500e8f4e80df2ef
qrxnz <send@qrxnz.dev>
2024-10-19 21:19:51 +0200

    removed extensions

--- snowy/home/vscode/default.nix
+++ snowy/home/vscode/default.nix
@@ -7,34 +7,5 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium-fhs;
-    extensions = with pkgs.vscode-extensions; [
-
-      #
-      # Theme
-      #
-
-
-      ########### The best like Polyphia the worst :P ####
-      catppuccin.catppuccin-vsc ##########################
-      catppuccin.catppuccin-vsc-icons ## catppuccin theme#
-      ####################################################
-
-      #
-      # Language extensions
-      #
-
-      ms-python.python # python
-      golang.go # golang
-      llvm-vs-code-extensions.vscode-clangd # c/c++
-
-      #
-      # Others
-      #
-
-      vscodevim.vim # vim support for vscode
-      yzhang.markdown-all-in-one # markdown shit
-      # rangav.vscode-thunder-client # api shit
-      # samgiz.vscode-kanbn-boards # kanban
-    ];
   };
 }
