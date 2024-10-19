ed432a95be64023346dedc2ee86cd9a477a23068
qrxnz <send@qrxnz.dev>
2024-10-19 21:18:25 +0200

    gtk theme

--- snowy/home/hyprland/default.nix
+++ snowy/home/hyprland/default.nix
@@ -32,6 +32,27 @@
       pkgs.cinnamon.nemo
     ];
 
+  # Gtk theme
+  gtk.theme = {
+    name = "catppuccin-mocha-blue-compact+default";
+    package =
+      (pkgs.catppuccin-gtk.overrideAttrs {
+        src = pkgs.fetchFromGitHub {
+          owner = "catppuccin";
+          repo = "gtk";
+          rev = "v1.0.3";
+          fetchSubmodules = true;
+          hash = "sha256-q5/VcFsm3vNEw55zq/vcM11eo456SYE5TQA3g2VQjGc=";
+        };
+      postUnpack = "";
+    }).override
+      {
+        accents = [ "sky" ];
+        variant = "mocha";
+        size = "compact";
+      };
+  };
+
   # Hyprland files
   home.file.".config/hypr".source = ../../../files/hypr;
   home.file.".config/hypr".recursive = true;
