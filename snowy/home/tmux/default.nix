e6e74cab1a7426edd74e1242b7bb29e22e8fec23
qrxnz <send@qrxnz.dev>
2024-10-24 16:43:54 +0200

    update

--- snowy/home/tmux/default.nix
+++ snowy/home/tmux/default.nix
@@ -21,7 +21,27 @@
     ];
 
 	extraConfig = ''
+    # Bar position
+    set-option -g status-position top
+
+    # Mouse on
 		set -g mouse on
+
+    # Base index
+    set -g base-index 1
+
+    # Renumber all windows when any window is closed
+    set -g renumber-windows on
+
+    # Set the control character to Ctrl+Spacebar (instead of Ctrl+B)
+    set -g prefix C-space
+    unbind-key C-b
+    bind-key C-space send-prefix
+
+    # Set new panes to open in current directory
+    bind c new-window -c "#{pane_current_path}"
+    bind '"' split-window -c "#{pane_current_path}"
+    bind % split-window -h -c "#{pane_current_path}"
 	'';
   };
 }
