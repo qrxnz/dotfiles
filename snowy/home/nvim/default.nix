afbfb1ebb66d91683974306eca77a7979aecf9fc
qrxnz <send@qrxnz.dev>
2024-10-29 09:48:53 +0100

    nveem refactor

--- snowy/home/nvim/default.nix
+++ snowy/home/nvim/default.nix
@@ -6,6 +6,23 @@
   system,
   ...
 }: {
-  #
-  home.packages = [ inputs.nveem.packages.x86_64-linux.default ];
+
+  home.packages = [
+    # flake.nix
+    #
+    #  inputs = {
+    #    nveem = {
+    #      url = "github:qrxnz/nveem";
+    #      # If using a stable channel you can use `url = "github:nix-community/nixvim/nixos-<version>"`
+    #      inputs.nixpkgs.follows = "nixpkgs";
+    #    };
+    #  };
+
+    inputs.nveem.packages.x86_64-linux.default # neovim
+
+    # lsp / formatter / dap
+    pkgs.go
+    pkgs.gopls
+    pkgs.delve # Go
+  ];
 }
