3920f56ee6b130fae34fbe244123c9fc2b558db4
qrxnz <send@qrxnz.dev>
2024-11-02 12:23:50 +0100

    enable sudo

--- modules/nixos/system/security/doas/default.nix
+++ modules/nixos/system/security/doas/default.nix
@@ -14,21 +14,21 @@ in {
 
   config = mkIf cfg.enable {
     # Disable sudo
-    security.sudo.enable = false;
+    security.sudo.enable = true;
 
     # Enable and configure `doas`.
-    security.doas = {
-      enable = true;
-      extraRules = [
-        {
-          users = [config.user.name];
-          noPass = true;
-          keepEnv = true;
-        }
-      ];
-    };
+    #security.doas = {
+      #enable = true;
+      #extraRules = [
+        #{
+          #users = [config.user.name];
+          #noPass = true;
+          #keepEnv = true;
+        #}
+      #];
+    #};
 
     # Add an alias to the shell for backward-compat and convenience.
-    environment.shellAliases = {sudo = "doas";};
+    #environment.shellAliases = {sudo = "doas";};
   };
 }
