a2b8f79aa9d8a77361d4e3347d3e94a5dfabcd66
qrxnz <send@qrxnz.dev>
2024-11-02 12:42:53 +0100

    git update

--- modules/nixos/apps/tools/git/default.nix
+++ modules/nixos/apps/tools/git/default.nix
@@ -26,11 +26,25 @@ in {
 
     environment.shellAliases = {
       # Git aliases
-      ga = "git add .";
-      gc = "git commit -m ";
-      gp = "git push -u origin";
+      gaa = "git add .";
+      gcm = "git commit -m";
+      gsu = "git submodule update --remote";
+      gsa = "git submodule add";
+      gpush = "git push -u origin";
+      gpull = "git pull";
+      grb = "git rebase";
+      grbc = "git rebase --continue";
+      gch = "git checkout";
+      grr = "git review -R";
 
       g = "lazygit";
     };
+
+    home.configFile."git/config".text = import ./config.nix {
+      sshKeyPath = "/home/${config.user.name}/.ssh/key.pub";
+      name = "qrxnz";
+      email = "send@qrxnz.dev";
+    };
+    home.configFile."lazygit/config.yml".source = ./lazygitConfig.yml;
   };
 }
