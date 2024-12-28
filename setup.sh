40586beb6635a63806de32d18e5642ba21f3c751
Karol Skoczyk <send@qrxnz.dev>
2024-12-28 17:49:34 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,55 +10,7 @@ if [[ $1 == "--hyprland-default" ]]; then
   cd $HOME
   echo "Installation Completed!"
 
-# Kali Linux Setup
-elif [[ $1 == "--kali-linux" ]]; then
-  # Install dependencies
-  sudo apt update && sudo apt upgrade -y
-  sudo apt-get install -y wget curl git thunar stow neovim podman zoxide fzf \
-    xsel xclip duf eza bat flameshot feh kitty tmux picom  xrdp nala\
-    golang delve clang ccls gdb cargo
-
-  # Prepare directories
-  mkdir -p ~/.local/bin/
-  mkdir -p ~/.local/share/fonts/
-
-  # Download & Install font
-  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip
-
-  unzip JetBrainsMono.zip -d ~/.local/share/fonts/
-
-  rm ./JetBrainsMono.zip
-
-  fc-cache -fv
-  
-  # Install bun (js runtime)
-  curl -fsSL https://bun.sh/install | bash
-
-  # Install starship
-  curl -sS https://starship.rs/install.sh | sh
-
-  # Install nix
-  sh <(curl -L https://nixos.org/nix/install) --no-daemon
-
-  # Install zplug (ZSH Plugin Manager)
-  curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-
-  # Sync dotfiles
-  stow files --adopt --ignore=hyprland --ignore=waybar --ignore=foot
-
-  echo "source ~/.config/zshrc/zshrc" > ~/.zshrc
-  
-  # Enable SSH
-  sudo systemctl enable ssh --now
-  
-  # Enable RDP
-  sudo systemctl enable xrdp --now
-  
-  cd $HOME
-  
-  echo "Installation Completed!"
-
 else
-  echo "Invalid argument. Try: ./setup.sh --hyprland-default or ./setup.sh --kali-linux"
+  echo "Invalid argument. Try: ./setup.sh --hyprland-default"
   exit 1
 fi
