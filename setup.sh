62e9036d0cf6727e87259045f164bfcb646c9e0f
Karol Skoczyk <send@qrxnz.dev>
2025-06-28 21:31:31 +0200

    Remove kali-setup

--- setup.sh
+++ setup.sh
@@ -58,12 +58,7 @@ elif [[ $1 == "--shell-only" ]]; then
   cd $HOME
   echo "Installation Completed!"
 
-elif [[ $1 == "--kali-lincox" ]]; then
-  cd kali-bspwm-setup &&\
-    chmod +x kali.sh root.sh && ./kali.sh &&\
-    sudo ./root.sh
-
 else
-  echo "Invalid argument. Try: ./setup.sh --hyprland-default || ./setup.sh --shell-only || ./setup.sh --macos || ./setup.sh --kali-lincox"
+  echo "Invalid argument. Try: ./setup.sh --hyprland-default || ./setup.sh --shell-only || ./setup.sh --macos"
   exit 1
 fi
